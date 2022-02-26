cee6cefa510ca9f3edce6404096eefe2a0853b7b
Maira-kh <76857838+Maira-kh@users.noreply.github.com>
2022-02-26 21:19:35 +1030

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,64 @@
+var hr, mn,sc;
+var scAngle,mnAngle,hrAngle;
 
+function setup() {
+  createCanvas(800,800);
+  angleMode(DEGREES)
+  
 
-function preload(){
-    
 }
 
-function setup(){
-   createCanvas(500,700)
-    
+function draw() {
+  background(0);  
+ 
+  translate(400,200)
+  rotate(-90)
+
+  hr = hour()
+  mn = minute()
+  sc = second()
+
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
+
+  push();
+  rotate(hrAngle);
+  stroke(255,255,0);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke(0,255,255);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
+  push();
+  rotate(scAngle);
+  stroke(255,0,255);
+  strokeWeight(5);
+  line(0,0,100,0);
+  pop();
+   
+ 
+  stroke(255,0,255); 
+  point(0,0) 
+  //drawing the arcs 
+  strokeWeight(10); 
+  noFill(); 
+  //Seconds 
+  stroke(255,0,255); 
+  arc(0,0,300,300,0,scAngle); 
+  //Minutes 
+  stroke(0,255,255); 
+  arc(0,0,280,280,0,mnAngle); 
+  //Hour 
+  stroke(255,255,0); 
+  arc(0,0,260,260,0,hrAngle);
+
+
+  drawSprites();
 }
-
-function draw(){
-    background(0)
-
-}   
-
